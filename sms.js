3e9983f599865704edb06b7c5c44ef7794409a34
Joeboy77 <136011166+Joeboy77@users.noreply.github.com>
2024-05-28 14:41:12 +0000

    updated

--- sms.js
+++ sms.js
@@ -3,13 +3,13 @@ const authToken = 'fbb88a1e5d1a8a742bafe9988a40e445';
 const client = require('twilio')(accountSid, authToken);
 
 // List of phone numbers you want to send SMS to
-const phoneNumbers = ['+233593930863', '+233534286320', '+233506480682', '+447442813679', '+233244840387', '+233593641702', '+233550675486', '+233545137705', '+233257220301'];
+const phoneNumbers = ['+233593930863', '+233534286320',  '+447442813679', '+233244840387', '+233593641702', '+233550675486', '+233545137705', '+233257220301'];
 
 // Your message
 const messageBody = 'Hi, fellow Board member of HP Radio, be remembered of our upcoming board meeting on saturday. Thank you';
 
 // Send SMS to each phone number in the list
-phoneNumbers.forEach((phoneNumber) => {
+phoneNumbers.forEach((...phoneNumber) => {
     client.messages
       .create({
          body: messageBody,
